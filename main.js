53f53292ac1fc8660b93d4904fd1310e1fc8062e
Kevin Torrington <kevin.torrington@administrators-MacBook-Pro.local>
2020-04-06 22:03:23 +0100

    refactored to json input

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-    var sound = "";
-    var url = "";
 function search() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
@@ -17,16 +14,9 @@ function search() {
         }
     }
 }
- 
 
-function play(sound) {
-        fetch("https://www.syrinscape.com/online/frontend-api/" + sound + "/play/?auth_token=1e93a1db5e982879f3a030972f5367cf0eefdf69");
-        url = "https://www.syrinscape.com/online/frontend-api/" + sound + "/play/?auth_token=1e93a1db5e982879f3a030972f5367cf0eefdf69" ;
-        
-        textToClipboard (url);
-    };   
 
-    function textToClipboard (url) {
+function textToClipboard(url) {
     var dummy = document.createElement("textarea");
     document.body.appendChild(dummy);
     dummy.value = url;
@@ -35,4 +25,26 @@ function play(sound) {
     document.body.removeChild(dummy);
 }
 
- 
+fetch("./sounds.json")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        preformLoop(data);
+
+    })
+
+function preformLoop(data) {
+    for (i = 0; i < data.length; i++) {
+        var line = document.createElement("LI");
+        line.className = "line";
+        var url = "\"https://www.syrinscape.com/online/frontend-api/" + data[i].Trigger + "/play/?auth_token=1e93a1db5e982879f3a030972f5367cf0eefdf69\""
+        line.innerHTML = "<a href='#0'  onclick='play(" + url + ")'> <b>" + data[i].Name + "</b> - " + data[i].Type + "</a>"
+        document.getElementById("myUL").appendChild(line);
+    }
+}
+
+function play(url) {
+    textToClipboard(url);
+    fetch(url);
+}
