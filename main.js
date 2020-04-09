468128dc936828a19f0c08359d0978e9069af8d4
Kevin Torrington <kevin.torrington@administrators-MacBook-Pro.local>
2020-04-09 02:12:30 +0100

    Added fav system

--- main.js
+++ main.js
@@ -35,16 +35,63 @@ fetch("./sounds.json")
     })
 
 function preformLoop(data) {
+
+
     for (i = 0; i < data.length; i++) {
-        var line = document.createElement("LI");
-        line.className = "line";
-        var url = "\"https://www.syrinscape.com/online/frontend-api/" + data[i].Trigger + "/play/?auth_token=1e93a1db5e982879f3a030972f5367cf0eefdf69\""
-        line.innerHTML = "<a href='#0'  onclick='play(" + url + ")'> <b>" + data[i].Name + "</b> - " + data[i].Type + "</a>"
-        document.getElementById("myUL").appendChild(line);
+
+
+        if (localStorage.getItem(data[i].ID) === "Fav") {
+            var line = document.createElement("LI");
+            var favIcon = document.createElement("LI")
+            favIcon.className = "line";
+            line.className = "line";
+            var url = "\"https://www.syrinscape.com/online/frontend-api/" + data[i].Trigger + "/play/?auth_token=1e93a1db5e982879f3a030972f5367cf0eefdf69\""
+            var sound = "\"" + data[i].ID + "\""
+            line.innerHTML = "<a href='#0'  onclick='play(" + url + ")'> <b>" + data[i].Name + "</b> - " + data[i].Type + "</a>"
+            favIcon.innerHTML = "<a href='#0'  onclick='removeFav(" + sound + ")'> <i class='fa fa-toggle-on' aria-hidden='true'></i>"
+            "</a>"
+            document.getElementById("myUL").appendChild(line);
+            document.getElementById("myUL").appendChild(favIcon);
+        }
     }
+    for (i = 0; i < data.length; i++) {
+
+
+        if (localStorage.getItem(data[i].ID) != "Fav") {
+            var line = document.createElement("LI");
+            var favIcon = document.createElement("LI");
+            favIcon.className = "line";
+            line.className = "li";
+            var url = "\"https://www.syrinscape.com/online/frontend-api/" + data[i].Trigger + "/play/?auth_token=1e93a1db5e982879f3a030972f5367cf0eefdf69\""
+            var sound = "\"" + data[i].ID + "\""
+            line.innerHTML = "<a href='#0'  onclick='play(" + url + ")'> <b>" + data[i].Name + "</b> - " + data[i].Type + "</a> <a href='#0'  onclick='fav(" + sound + ")'>   <i class='fa fa-toggle-off' aria-hidden='true'></i> "
+            "</a>";
+
+            document.getElementById("myUL").appendChild(line);
+
+        }
+    }
+
 }
 
 function play(url) {
     textToClipboard(url);
     fetch(url);
+
+    /*
+        localStorage.setItem("m:16595", "Fav");
+        localStorage.setItem("m:16596", "Fav");
+        localStorage.setItem("m:16597", "Fav");
+        localStorage.setItem("m:16598", "Fav");
+        localStorage.setItem("m:16602", "Fav"); */
+}
+
+function fav(sound) {
+    localStorage.setItem(sound, "Fav");
+
+}
+
+function removeFav(sound) {
+    localStorage.removeItem(sound, "Fav");
+
 }
